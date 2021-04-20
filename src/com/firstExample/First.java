cf4aeda9076fa9138b0213bc6546634a046f3892
Vladimir <mkkinj@gmail.com>
2021-04-20 21:07:08 +0300

    init project

---
+++ src/com/firstExample/First.java
@@ -0,0 +1,12 @@
+package com.firstExample;
+
+public class First {
+    public static void main(String[] args) {
+        String greeting = "Hello world";
+        System.out.println(greeting);
+        World world = new World();
+        System.out.println(world.getWorldGreeting());
+    }
+}
+
+
---
+++ src/com/firstExample/World.java
@@ -0,0 +1,8 @@
+package com.firstExample;
+
+public class World {
+    private static String worldGreeting = "HELLO";
+
+    public static String getWorldGreeting(){return worldGreeting;}
+
+}
